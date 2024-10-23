6b72810a0bf059f769bd3e6b99c9522ed43effe6
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-10-23 19:22:45 +0530

    chore: minor-bug-fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        // domains: ['assets.aceternity.com'],
+      },
+    ],
+  },
+};
 
 export default nextConfig;
